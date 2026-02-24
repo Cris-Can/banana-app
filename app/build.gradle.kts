48f2036cde8500975e1c125e1e7e42ac89704d2b
cristian cantillana <cristian.zhp@gmail.com>
2026-02-23 23:50:08 -0300

    release: bump to v1.2.1 (versionCode 15) for Play Console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.eventos.banana"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.2.0"
+        versionCode = 15
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
